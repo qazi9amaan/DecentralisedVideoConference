11e975f72712e2ab700335be27da14ef10d01642
qazi <qazi9amaan@gmail.com>
2020-11-30 14:16:22 +0530

    solved bugs

--- src/app/services/socketio.service.ts
+++ src/app/services/socketio.service.ts
@@ -7,7 +7,7 @@ import { Observable } from "rxjs";
 export class SocketioService {
     private socket: any;
   constructor() {
-    this.socket = io("http://localhost:3000");
+    this.socket = io("https://aselpanda.ml/");
   }
 
   listen(eventname: string) : Observable<any> {
